85d71f223ee6e3f0e30da80579cd1fc409bad22f
Paul Booth <thephantompaulbooth@gmail.com>
2013-01-29 23:03:50 -0500

    added logging for admin

--- device_fbid_server.js
+++ device_fbid_server.js
@@ -37,7 +37,9 @@ app.configure('development', function(){
 
 app.get('/:fbapp/admin', function(req, res) {
   getAllFbUsers(req.params.fbapp, function(dbitems) {
-    res.render('namespace', {namespace:req.params.fbapp, fbusers: dbitems.map(function(dbitem) { return dbitem.fbuser; })});
+    var fbusers = dbitems.map(function(dbitem) { return dbitem.fbuser; });
+    console.log(JSON.stringify(fbusers, undefined, 2));
+    res.render('namespace', {namespace:req.params.fbapp, fbusers: fbusers});
   });
 });
 
